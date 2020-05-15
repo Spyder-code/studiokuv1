b2e05480402e04796ce239466e309c3a5d3264f4
Spyder-code <luaysyauqillah@gmail.com>
2020-05-15 09:54:30 +0700

    perubahan services

--- config/services.php
+++ config/services.php
@@ -14,22 +14,6 @@ return [
     |
     */
 
-    // 'mailgun' => [
-    //     'domain' => env('MAILGUN_DOMAIN'),
-    //     'secret' => env('MAILGUN_SECRET'),
-    //     'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-    // ],
-
-    // 'postmark' => [
-    //     'token' => env('POSTMARK_TOKEN'),
-    // ],
-
-    // 'ses' => [
-    //     'key' => env('AWS_ACCESS_KEY_ID'),
-    //     'secret' => env('AWS_SECRET_ACCESS_KEY'),
-    //     'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    // ],
-
     'google' => [
         'client_id' => '108216179335-adhec7nfgml53e3sjcv5flbbhtfun3aq.apps.googleusercontent.com',
         'client_secret' => 'gRYHGWzvX4C95inbyVtUH5xI',
